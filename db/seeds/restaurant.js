829f095e393230cc61cc37a2cfc385a2c4d26139
Kai Tang <16kt16@queensu.ca>
2017-10-26 16:01:49 +0000

    add email for users and description for restaurant

--- db/seeds/restaurant.js
+++ db/seeds/restaurant.js
@@ -2,7 +2,7 @@ exports.seed = function(knex, Promise) {
   return knex('restaurant').del()
     .then(function () {
       return Promise.all([
-        knex('restaurant').insert({name:'Red Lobster', address:'1234 Fitch Ave. Toronto, ON M2M 7Z1', phone_number: '+16134566789', schedule: 'Mon. to Sun. 11AM-10PM',type: 'fast food', picture: 'restaurant1.jpeg'})
+        knex('restaurant').insert({name:'Red Lobster', address:'1234 Fitch Ave. Toronto, ON M2M 7Z1', phone_number: '+16134566789', schedule: 'Mon. to Sun. 11AM-10PM',type: 'fast food', picture: 'restaurant1.jpeg',description:`Our seafood buyers and inspectors carefully check for water cleanliness, seafood quality andsafety in over 20 countries`})
       ]);
     });
 };
